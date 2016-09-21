be6663b83935c5cfc85c92c5aee2acc95eb7f801
James DeFrancesco <jdefrancesco2@gmail.com>
2016-09-20 20:48:45 -0400

    adds new styling (pre user auth)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803205838) do
+ActiveRecord::Schema.define(version: 20160917033743) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -18,5 +18,6 @@ ActiveRecord::Schema.define(version: 20160803205838) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 
 end

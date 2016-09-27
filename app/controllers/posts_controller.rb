cd50dd24374d3192b0d13e960c36574195002fac
James DeFrancesco <jdefrancesco2@gmail.com>
2016-09-27 11:49:44 -0400

    fixes user auth

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_filter :authenticate_user!
 
   def index
     @posts = Post.all.order("created_at DESC")
@@ -21,6 +21,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
